2ca10e90dc34bc317c23e49333a97c26b3390e4c
happyiw <123283921+happyiw@users.noreply.github.com>
2025-07-06 19:31:22 +1000

    mysql enabled, 4 new paths added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mySql = require("mysql2");
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -32,6 +33,81 @@ app.get('/dynamic', (req, res) => {
     body: result
   });
 });
+
+
+
+
+const connection = mySql.createConnection({
+	host: "localhost",
+	user: "root",
+	database: "chatbottests",
+	password: 'admin'
+});
+
+
+app.get('/getAllItems', (req, res) => {
+	connection.query("SELECT * FROM items", (error, results) => {
+		if (error) {
+			res.send(null);
+		} else {
+			res.send(results); 
+		}
+	});
+});
+
+app.post('/addItem', (req,res)=>{
+	const {name, desc} = req.query;
+	if (!name || !desc) {
+		return res.send(null);
+	}
+
+	connection.query("INSERT INTO items(name, `desc`) VALUES(?, ?)", [name, desc], function(err, results) {
+		if (err) { 
+			return res.send(null);
+		}
+		res.send({name,desc});
+	});
+});
+
+app.post('/deleteItem', (req, res) => {
+	const { id } = req.query;
+	if (isNaN(parseFloat(id)) || !isFinite(id)) {
+		return res.send(null); 
+	}
+	const numberID = Number(id);
+
+	connection.query("DELETE FROM items WHERE id = ?", [numberID], function(err, results) {
+		if (err) {
+		  return res.send(null); 
+		}
+		if (results.affectedRows === 0) {
+		  return res.send({}); 
+		}
+		res.send({ id: numberID }); 
+	});
+});
+
+app.post('/updateItem', (req, res) => {
+	const { id, name, desc } = req.query;
+	if (isNaN(parseFloat(id)) || !isFinite(id) || !name || !desc) {
+		return res.send(null);
+	}
+
+	const numberID = Number(id);
+
+	connection.query("UPDATE items SET name = ?, `desc` = ? WHERE id = ?", [name, desc, numberID], function (err, results) {
+		if (err) {
+			return res.send(null); 
+		}
+		if (results.affectedRows === 0) {
+			return res.send({}); 
+		}
+		res.send({id: numberID,name,desc});
+	});
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
