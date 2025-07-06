f87d7fa294372affaed1bf0925c8f1d9e95e0585
happyiw <123283921+happyiw@users.noreply.github.com>
2025-07-06 18:35:39 +1000

    Express enabled + few paths coded

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const server = http.createServer(function(request,response){
-    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    response.end("<h1>Привет, октагон!</h1>");
+app.get('/', (req, res) => {
+	res.send('<h1>Hello, Octagon! This is Express!</h1>');
 });
 
-server.listen(3000, () => {
-    console.log('Сервер запущен на http://localhost:3000');
+app.get('/static', (req, res) => {
+  res.send({
+    header: "Hello",
+    body: "Octagon NodeJS Test"
+  });
+});
+
+app.get('/dynamic', (req, res) => {
+  const { a, b, c } = req.query;
+
+  if (
+    isNaN(parseFloat(a)) || !isFinite(a) ||
+    isNaN(parseFloat(b)) || !isFinite(b) ||
+    isNaN(parseFloat(c)) || !isFinite(c)
+  ) {
+    return res.send({ header: "Error" });
+  }
+  const numberA = parseFloat(a);
+  const numberB = parseFloat(b);
+  const numberC = parseFloat(c);
+  const result = (numberA * numberB * numberC) / 3;
+  res.send({
+    header: "Calculated",
+    body: result
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
